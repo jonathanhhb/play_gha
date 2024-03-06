8ef6bb6b4c03b1faed4efa23fd3606d0ceeffaeb
Jonathan Bloedow <jhb4@cornell.edu>
2024-03-06 15:25:31 -0800

    Trying to build on win.

--- my_extension.c
+++ my_extension.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <Python.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+PyMODINIT_FUNC PyInit_my_extension(void) {
+    return NULL; // or PyModule_Create(&module_definition); if you have a module definition
+}
 
 void call_me()
 {
